ca4992131bc227dc47187af9e32e40c4022845ca
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:29:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+"use strict";
+//alien color is green
+//let alien_color = "green";
+//if (alien_color=== 'green'){
+// console.log("player just earn 5 point.")
+//}
+//else if (alien_color=== 'yellow'){
+//console.log("player just earned 10 points.")
+//}
+//else if (alien_color=== 'red'){
+// console.log("player just earned 15 points.")
+//}
+//alien color is yellow
+///let alien_color = "yellow";
+//if (alien_color=== 'green'){
+//console.log("player just earn 5 point.")
+//}
+//else if (alien_color=== 'yellow'){
+//console.log("player just earned 10 points.")
+//}
+//else if (alien_color=== 'red'){
+//console.log("player just earned 15 points.")
+//}
+//alien color is red
+let alien_color = "red";
+if (alien_color === 'green') {
+    console.log("player just earn 5 point.");
+}
+else if (alien_color === 'yellow') {
+    console.log("player just earned 10 points.");
+}
+else if (alien_color === 'red') {
+    console.log("player just earned 15 points.");
+}
